223eb90053c4b4c22ea046432c743642be993aad
shuhail <shuhail@nimbly.consulting>
2025-05-05 04:34:40 +0600

    remove missing link

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -23,7 +23,7 @@ const Contact = () => {
         <div className="relative mx-auto max-w-c-1390 px-7.5 pt-10 lg:px-15 lg:pt-15 xl:px-20 xl:pt-20">
           <div className="absolute left-0 top-0 -z-1 h-2/3 w-full rounded-lg bg-linear-to-t from-transparent to-[#dee7ff47] dark:bg-linear-to-t dark:to-[#252A42]"></div>
           <div className="absolute bottom-[-255px] left-0 -z-1 h-full w-full">
-            <Image
+            {/* <Image
               src="../images/shape/shape-dotted-light.svg"
               alt="Dotted"
               className="dark:hidden"
@@ -34,7 +34,7 @@ const Contact = () => {
               alt="Dotted"
               className="hidden dark:block"
               fill
-            />
+            /> */}
           </div>
 
           <div className="flex flex-col-reverse flex-wrap gap-8 md:flex-row md:flex-nowrap md:justify-between xl:gap-20">
